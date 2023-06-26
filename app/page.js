20c840d7d360ea49b04a1f1c1eb703a769a5e795
JonnathanE <jonathan684j@gmail.com>
2023-06-26 02:33:18 -0500

    feature/R01-home-page

--- app/page.js
+++ app/page.js
@@ -1,95 +1,133 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "@/styles/home.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+      <section className={styles.parallax_outer}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          alt="cielo azul"
+          src="/images/sky-gris.png"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className={`${styles.image_parallax} ${styles.sky}`}
         />
-      </div>
+        <Image
+          alt="casa de la lira"
+          src="/images/home.png"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className={`${styles.image_parallax} ${styles.home_lira}`}
+        />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <Image
+          alt="logo lira"
+          src="/images/logolira.png"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className={`${styles.image_parallax} ${styles.logo_lira}`}
+        />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        <div className={`${styles.image_parallax} ${styles.title_wrapp}`}>
+          <h1 className={styles.title}>
+            Proyecto casa de la lira Universidad del Azuay
+          </h1>
+          <p className={styles.subtitle}>Conoce su historia</p>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+      <section
+        className={`${styles.sections_wrapp} ${styles.second_section} ${styles.section_padding}`}
+      >
+        <div className={styles.text_wrapper}>
+          <p className={`${styles.text}`}>
+            La Casa de la Lira es una reconocida construcción ubicada en la
+            ciudad de Cuenca, en Ecuador.
           </p>
-        </a>
-      </div>
+          <p className={`${styles.text}`}>
+            Es considerada un importante patrimonio cultural de la ciudad y es
+            conocida por su arquitectura y valor histórico.
+          </p>
+        </div>
+        <Image
+          alt="casa de la lira"
+          src="/images/second-home.png"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className={`${styles.second_image}`}
+        />
+        <div
+          className={`${styles.banner_bottom_wrapper} ${styles.image_bottom_cirle}`}
+        ></div>
+      </section>
+
+      <section className={`${styles.third_section}`}>
+        <div className={`${styles.back_banner_1}`}>
+          <div className={`${styles.section_padding} ${styles.section_felx}`}>
+            <h2 className={styles.subtitles}>Época Dorada</h2>
+            <p className={`${styles.text} ${styles.text_spacing_bottom}`}>
+              1860-1900: La Casa de la Lira es testigo de la efervescencia
+              cultural y literaria de Cuenca durante la época conocida como "La
+              Edad de Oro". Se convirtió en un espacio para la creación
+              artística, se impartían clases de piano y otros instrumentos.
+              Reconocidos escritores y poetas, como Juan Montalvo y José J.
+              Coba, frecuentan la casa y participan en tertulias literarias que
+              ayudan a forjar la identidad cultural de la región.
+            </p>
+          </div>
+          <div
+            className={`${styles.banner_bottom_wrapper} ${styles.image_bottom_squares}`}
+          ></div>
+        </div>
+      </section>
+
+      <section className={`${styles.fourth_section}`}>
+        <div className={`${styles.back_banner_2}`}>
+          <div className={`${styles.section_padding} ${styles.section_felx}`}>
+            <h2 className={styles.subtitles}>Abandono</h2>
+            <p className={`${styles.text} ${styles.text_spacing_bottom}`}>
+              En 1940, a los 93 años, y en la misma casa donde nació, aprendió y
+              enseñó su música, y la de los grandes maestros, falleció don José
+              María Rodríguez, orgullo de la Morlaquia y patrono del
+              Conservatorio de esta ciudad. Luego de la muerte de José María
+              Rodríguez, la Casa de la Lira pasó por un período de abandono y
+              deterioro.
+            </p>
+          </div>
+          <div
+            className={`${styles.banner_bottom_wrapper} ${styles.image_bottom_flames}`}
+          ></div>
+        </div>
+      </section>
+
+
+      <section className={`${styles.fifth_section}`}>
+        <div className={`${styles.back_banner_3}`}>
+          <div className={`${styles.section_padding} ${styles.section_felx}`}>
+            <h2 className={styles.subtitles}>Incendio</h2>
+            <p className={`${styles.text} ${styles.text_spacing_bottom}`}>
+            1978: Incendio de la casa, solo se logró conservar la fachada. A pesar de ello, sigue siendo un lugar con valor histórico y cultural en Cuenca, y se llevan a cabo esfuerzos de restauración para preservar su patrimonio arquitectónico.
+            </p>
+          </div>
+        </div>
+      </section>
+
+
+      <section className={`${styles.sixth_section}`}>
+        <div className={`${styles.back_banner_4}`}>
+          <div className={`${styles.section_padding} ${styles.section_felx}`}>
+            <h2 className={styles.subtitles}>Actualidad</h2>
+            <p className={`${styles.text} ${styles.text_spacing_bottom}`}>
+            2000-presente: La Casa de la Lira se convierte en un importante centro cultural en Cuenca, albergando exposiciones, conciertos, presentaciones literarias y otras actividades culturales. Es considerada un ícono de la historia y la identidad cultural de la ciudad, y sigue siendo un lugar de encuentro para la comunidad artística y literaria de Cuenca.
+            </p>
+          </div>
+        </div>
+      </section>
+
     </main>
-  )
+  );
 }
